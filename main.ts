6f568a7e6a1e10945555b4def916d14d4254c0ce
C0DEFpS <122454759+C0DEFpS@users.noreply.github.com>
2023-01-11 12:56:01 +0000

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,53 @@
+let numero = 0
+input.onGesture(Gesture.Shake, function () {
+    numero = randint(1, 6)
+    if (numero == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (numero == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (numero == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (numero == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (numero == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (numero == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
